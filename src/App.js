c73b748d2a1f53b40644aed2d747a3c91ce0483d
pardedeputra27 <pardedeputra27@gmail.com>
2022-11-14 09:54:21 +0700

    clicked event

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from "./components/Header";
  
 function App() {
+ 
+  const clickMe = () => {
+    console.log('Clicked');
+  }
+ 
   return (
     <div>
       <Header />
+      <button onClick={ clickMe }>Click Me</button>
     </div>
   );
 }
