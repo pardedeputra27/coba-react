3ed43a70273b218597d151237e6fc13ec8487a5a
pardedeputra27 <pardedeputra27@gmail.com>
2022-11-14 16:54:47 +0700

    update root

--- src/components/productList.js
+++ src/components/productList.js
@@ -1,15 +1,18 @@
 const ProductList = ({ products, deleteProduct }) => {
-        return (
-            <div>
-                <ul>
-                    {products.map((product) => (
-                    <li key={ product.id }> { product.title } - { product.price } 
-                        <button onClick={ () => deleteProduct(product.id) }>Delete</button>
-                    </li>
-                    ))}
-                </ul>
-            </div>
-        )
-    }
-     
-    export default ProductList
+  return (
+    <div class="coba">
+                  
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.title} - {product.price}
+            <button onClick={() => deleteProduct(product.id)}>Delete</button>                
+          </li>
+        ))}
+                    
+      </ul>
+              
+    </div>
+  );
+};
+export default ProductList;
