338b49add66c0624cc0b4001f35805d6e7703462
pardedeputra27 <pardedeputra27@gmail.com>
2022-11-17 09:27:54 +0700

    key must only be unique among siblings

--- src/App.js
+++ src/App.js
@@ -4,18 +4,36 @@ import "./App.css";
 import React from "react";
 
 
+function Blog(props) {
+  const sidebar = (    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+  const content = props.posts.map((post) =>    <div key={post.id}>      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+  return (
+    <div>
+      {sidebar}      <hr />
+      {content}    </div>
+  );
+}
 
 
 function App() {
-  const numbers = [1, 2, 3, 4, 5];
-  const listItems = numbers.map((number) =>
-  <li key={number.toString()}>    {number}
-    </li>
-  );
+  
+  const posts = [
+    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+    {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+  ];
   return (
-    <ul>
-      {listItems}
-    </ul>
+    <div>
+      <Blog posts={posts} />
+    </div>
   );
 }
 
