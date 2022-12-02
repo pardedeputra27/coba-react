e73100ec82e8272813a02a38cf6a53203202da5a
pardedeputra27 <pardedeputra27@gmail.com>
2022-12-02 16:40:10 +0700

    Send data with post method

--- src/container/API/BlogPost.jsx
+++ src/container/API/BlogPost.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import "./BlogPost.css"
+import "./BlogPost.css";
+import "./form.css"
 import Post from "./Post";
 import axios from "axios";
 
@@ -7,6 +8,12 @@ class BlogPost extends React.Component{
     state ={
         post:[],
         failed:true,
+        formBlogPost:{
+            userId:1,
+            id :1,
+            title:"this is Title",
+            body:"this is Body"
+        }
     }
 
     componentDidMount(){
@@ -22,7 +29,7 @@ class BlogPost extends React.Component{
     }
 
     renderAPI =()=>{
-        axios.get('http://localhost:3001/posts')
+        axios.get('http://localhost:3001/posts?_sort=id&_order=desc')
         .then((response)=> {
             //console.log(response);
             this.setState({
@@ -38,11 +45,44 @@ class BlogPost extends React.Component{
                 this.renderAPI();
             });
     }
+
+    handleOnChange =(e)=>{
+        let formBlogPostNew = {...this.state.formBlogPost};
+        let timeStamp = new Date().getTime();
+        formBlogPostNew[e.target.name] = e.target.value;
+        formBlogPostNew['id'] = timeStamp;
+       // console.log(timeStamp);
+        this.setState({
+            formBlogPost:formBlogPostNew
+        },()=>{
+            //console.log(this.state.formBlogPost);
+        })
+        
+    }
+
+    handleSubmit =()=>{
+        axios.post('http://localhost:3001/posts',this.state.formBlogPost)
+        .then((response)=> {
+             this.renderAPI();
+            console.log(response);
+          })
+        .catch((error)=> {
+            console.log(error);
+          });
+    }
     render(){
         return(
 
             <React.Fragment>
             <p className="section-title">Blog post</p>
+            <div className="container-form">
+                <label >Insert Title</label>
+                <input type="text" id="fname" name="title" placeholder="Your title.." onChange={this.handleOnChange} />
+                <label>Inser Body</label>
+                <input type="text" id="lname" name="body" placeholder="Your body.." onChange={this.handleOnChange}/>
+                <button onClick={this.handleSubmit} className ="button-submit">SUBMIT</button>
+            </div>
+
             {
             this.state.failed?<h5 className="section-title">Loading...</h5>:this.state.post.map((value,index,array)=>{
                 //console.log('value',value.id)
